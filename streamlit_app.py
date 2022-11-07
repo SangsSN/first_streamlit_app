ce00e47d23aa2c15f777da5f0449da36bc2ff4ec
Sangs <117344154+SangsSN@users.noreply.github.com>
2022-11-07 14:31:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ from urllib.error import URLError
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favourites')
+
 streamlit.text('🥣Omega3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale,Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-range Egg')
