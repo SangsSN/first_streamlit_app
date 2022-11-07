f43b530c5a6158eb05779b7a519447f377aa14a2
Sangs <117344154+SangsSN@users.noreply.github.com>
2022-11-07 14:29:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,11 +24,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 #new section to display fruityvice api response 
 
-streamlit.header('FruityVice Fruit Advice!') 
+
 #create the repeatable code block(called a function)
 def get_fruityvice_data(this_fruit_choice):
    
-        fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+        fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
       #take the json version of the response and normalise it 
         fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
         return fruityvice_normalized
@@ -45,29 +45,33 @@ try:
 except URLError as e:
    streamlit.error()
       
+#don't run anything past here while we trouble shoot 
+streamlit.stop()
 
 
 my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
-
 my_cur=my_cnx.cursor()
-
 my_cur.execute("SELECT * FROM fruit_load_list")
-
 my_data_rows=my_cur.fetchall()
-
-streamlit.header("the fruit load list contains::")
-
+streamlit.header("The fruit load list contains::")
 streamlit.dataframe(my_data_rows)
-
-add_my_fruit=streamlit.text_input('What fruit would you like to add?','kiwi')
-
+add_my_fruit=streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thank you for adding',add_my_fruit)
-
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
-#don't run anything past here while we trouble shoot 
 
-streamlit.stop()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
